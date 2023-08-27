fed1e0ebf66eb5f0d67fa831d71366bb9eec8609
Madu-f0r0 <duherbert@gmail.com>
2023-08-27 16:07:42 -0700

    Handled zero top element for mod opcode

--- mod.c
+++ mod.c
@@ -18,7 +18,7 @@ void mod(stack_t **stack, unsigned int line_number)
                 exit(EXIT_FAILURE);
         }
 
-	if (*stack == 0)
+	if ((*stack)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
